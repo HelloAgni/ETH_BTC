4fcc890697404d29cf47802500c0e915cb761795
Байбак Кирилл lin <zloifox@gmail.com>
2023-04-05 14:23:36 +0300

    fix: readme, delete prints, added logging

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ETH_BTC
 
+**Script** that determines the own price movement of the **ETHUSDT** futures,  
+excluding from them the movements caused by the influence of the **BTCUSDT** price  
+
 ***How to start***  
 ```bash
 git clone <project>
@@ -12,5 +15,22 @@ python -m venv venv
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+python ethusdt.py 
 #info.log
+```
+Set the script duration if needed
+```bash
+TEST_SCRIPT_DURATION = 65  # 65 seconds
+SCRIPT_DURATION = 3900  # 65 minutes
+...
+async def main():
+    ...
+    await asyncio.sleep(TEST_SCRIPT_DURATION)  # TIME SCRIPT
+    ...
+```
+Example output
+```bash
+INFO     2023-04-05 14:04:31 Analyzing ETHUSDT change percent is: -0.049 for last hour
+WARNING  2023-04-05 14:04:31 Analyzing ETHUSDT corellation is: 0.099 for last hour
+WARNING  2023-04-05 14:04:31 Price movement detected
 ```
--- ethusdt.py
+++ ethusdt.py
@@ -1,9 +1,13 @@
 import asyncio
+import logging
 import statistics
+import sys
 from datetime import datetime, timedelta
+
 from binance import AsyncClient
 
-SCRIPT_DURATION = 3600  # 60 minutes
+TEST_SCRIPT_DURATION = 65  # 65 seconds
+SCRIPT_DURATION = 3900  # 65 minutes
 CORRELATION = 0.6  # for example 0.6; Usual correlation ~ 0.98
 SLEEP_1HOUR = 3600  # 60 minutes
 SLEEP_1MINUTE = 60  # 1 minute
@@ -15,16 +19,26 @@ PERIOD_MINUTE = 'minute'
 PERIOD_START = 'script_launch'
 DETECTED = 'Price movement detected'
 
+file_handler = logging.FileHandler(filename='info.log')
+stdout_handler = logging.StreamHandler(stream=sys.stdout)
+handlers = [file_handler, stdout_handler]
 
-async def check_corel(eth, btc, period=None):
+logging.basicConfig(
+    format="%(levelname)-8s %(asctime)s %(message)s",
+    level=logging.INFO,
+    handlers=handlers,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+async def check_corel(eth: list, btc: list, period=None):
     """
-    Analyzing the start_current_price.
+    Analyzing the corellation.
     """
     corel = round(statistics.correlation(eth, btc), 3)
-    print(f'Analyzing {ETH} corellation is: '
-          f'{corel} for last {period}')
     if corel <= CORRELATION:  # 0.6
-        print(DETECTED)
+        logging.warning(f'Analyzing {ETH} corellation is: '
+                        f'{corel} for last {period}')
         return True
     return False
 
@@ -40,30 +54,28 @@ async def start_current_price(client: AsyncClient):
     start_price_bt = float(bt.get("price"))
     while True:
         g = await client.get_symbol_ticker(symbol=ETH)
-
         curent_price_eth = float(g.get("price"))
-        print(f'Ticker: {g.get("symbol")} '
-              f'current price: {float(g.get("price"))}')
+        logging.info(f'Ticker: {g.get("symbol")} '
+                     f'current price: {float(g.get("price"))}')
         eth_start_end = [start_price_eth, curent_price_eth]
         change_percent = round(
-            ((curent_price_eth - start_price_eth) / start_price_eth) * 100, 2
+            ((curent_price_eth - start_price_eth) / start_price_eth) * 100, 3
             )
         bt = await client.get_symbol_ticker(symbol=BTC)
         curent_price_bt = float(bt.get("price"))
         btc_start_end = [start_price_bt, curent_price_bt]
         if change_percent <= -1 or change_percent >= 1:
-            print(f'ETHUSD change percent is: {change_percent}')
             result = await check_corel(
                 eth=eth_start_end,
                 btc=btc_start_end,
                 period=PERIOD_START
                 )
             if result:
-                print(DETECTED)
+                logging.warning(DETECTED)
         await asyncio.sleep(SLEEP_15SECONDS)
 
 
-async def percent_and_corel(eth, btc, period: str):
+async def percent_and_corel(eth: list, btc: list, period: str):
     """
     Analyzing the price change in percentages
     Analyzing the correlation.
@@ -72,15 +84,14 @@ async def percent_and_corel(eth, btc, period: str):
     open_price = eth[0]
     close_price = eth[-1]
     change_percent = round(
-        ((close_price - open_price) / open_price) * 100, 2
+        ((close_price - open_price) / open_price) * 100, 3
         )
-    print(f'Analyzing {ETH} change percent is: '
-          f'{change_percent} for last {period}')
+    logging.info(f'Analyzing {ETH} change percent is: '
+                 f'{change_percent} for last {period}')
     if change_percent <= -1 or change_percent >= 1:
         corel = statistics.correlation(btc, eth)
-        print('Correlation: ', corel)
         if corel <= CORRELATION:  # 0.6
-            print('Own price movement!')
+            # print(DETECTED)
             return True
     return False
 
@@ -110,17 +121,13 @@ async def analyzing_correlation_1hour(client: AsyncClient):
             )
         full_price_list_eth = [
             elem for inner in eth for elem in map(float, inner[1:5:3])]
-        # print('FULL BTC', full_price_list_btc)
-        # print('FULL ETH', full_price_list_eth)
-        # FULL BTC [28481.39, 28476.21, 28476.21, 28485.98, 28485.98, 28490.82.....
-        # FULL BTC [1909.56, 1909.42, 1909.42, 1909.4, 1909.4, 1909.95.....
         result = await check_corel(
             eth=full_price_list_eth,
             btc=full_price_list_btc,
-            # period=PERIOD_HOUR
+            period=PERIOD_HOUR
             )
         if result:
-            print(f'price changed more than 1% for last: {PERIOD_HOUR}')
+            logging.warning(DETECTED)
         await asyncio.sleep(SLEEP_1HOUR)
         cur_serv_time = cur_serv_time + timedelta(hours=1)
 
@@ -154,7 +161,7 @@ async def price_movement_change_1hour(client: AsyncClient):
             period=PERIOD_HOUR
             )
         if result:
-            print(f'price changed more than 1% for last: {PERIOD_HOUR}')
+            logging.warning(DETECTED)
         await asyncio.sleep(SLEEP_1HOUR)
         cur_serv_time = cur_serv_time + timedelta(hours=1)
 
@@ -188,7 +195,7 @@ async def price_movement_change_1min(client: AsyncClient):
             period=PERIOD_MINUTE
             )
         if result:
-            print(f'price changed more than 1% for last: {PERIOD_MINUTE}')
+            logging.warning(DETECTED)
         await asyncio.sleep(SLEEP_1MINUTE)
         cur_serv_time = cur_serv_time + timedelta(minutes=1)
 
@@ -196,21 +203,22 @@ async def price_movement_change_1min(client: AsyncClient):
 async def main():
     client = await AsyncClient.create()
 
-    # task_infinity_price = asyncio.Task(start_current_price(client))
+    task_infinity_price = asyncio.Task(start_current_price(client))
     task_corel_1hour = asyncio.Task(price_movement_change_1hour(client))
-    # task_corel_1m = asyncio.Task(price_movement_change_1min(client))
+    task_corel_1m = asyncio.Task(price_movement_change_1min(client))
     task_full_analyz = asyncio.Task(analyzing_correlation_1hour(client))
-    await asyncio.sleep(30)  # TIME SCRIPT
+    await asyncio.sleep(TEST_SCRIPT_DURATION)  # TIME SCRIPT
 
     await client.close_connection()
-    
+
     task_full_analyz.cancel()
-    # task_infinity_price.cancel()
+    task_infinity_price.cancel()
     task_corel_1hour.cancel()
-    # task_corel_1m.cancel()
+    task_corel_1m.cancel()
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     loop.close()
+    logging.info('---The End---\n')
